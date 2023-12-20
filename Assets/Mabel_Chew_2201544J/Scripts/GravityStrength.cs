28c636784c52280f1355f552cabe0fa0c28533ac
imvsalty <105273716+imvsalty@users.noreply.github.com>
2023-12-20 12:33:15 +0800

    Revert "starting over with a for loop instead"
    This reverts commit c53f4905de2e3ea2b3cfd5f3839d0ca7fc670707.

--- Assets/Mabel_Chew_2201544J/Scripts/GravityStrength.cs
+++ Assets/Mabel_Chew_2201544J/Scripts/GravityStrength.cs
@@ -4,53 +4,98 @@ using UnityEngine;
 
 public class GravityStrength : MonoBehaviour
 {
-    public GravitySwitch gravitySwitch;
-    Rigidbody rb;
+    //public GravitySwitch gravitySwitch;
+    //Rigidbody rb;
 
-    public float gravityStrength = 9.81f;
-    public float maxGravityStrength = 20f;
-    public float gravityChangeDuration = 20f;
-    public float currentStrength;
-    // Start is called before the first frame update
+    //public float gravityStrength = 9.81f;
+    //public float maxGravityStrength = 20f;
+    //public float gravityChangeDuration = 20f;
+    //public float currentStrength;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    SetGravityStrength(gravityStrength);
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //when jump is pressed
+    //    //start a couroutine
+    //    //the time hold is the gravity strength scale, set a max limit too
+    //}
+
+    //public IEnumerator ChangeGravityStrength(float maxStrength, float duration)
+    //{
+    //    float initialStrength = Physics.gravity.magnitude;
+    //    float timeElapsed = 0f;
+    //    float totalLerpTime = 20f;
+
+
+    //    while (initialStrength <= maxStrength)
+    //    {
+    //        currentStrength = Mathf.Lerp(initialStrength, maxStrength, timeElapsed/totalLerpTime);
+    //        SetGravityStrength(currentStrength);
+    //        timeElapsed += Time.deltaTime;
+    //        yield return null;
+    //    }
+
+    //    SetGravityStrength(maxStrength);
+    //}
+
+    //public void SetGravityStrength(float strength)
+    //{
+    //    Physics.gravity = gravitySwitch.gravityDir[gravitySwitch.gravityDirIndex].normalized * strength;
+    //}
+
+    //public float GetGravityStrength()
+    //{
+    //    return Physics.gravity.magnitude;
+    //}
+
+    Rigidbody rb;
+    bool isJumpButtonPressed = false;
+
+    // Adjust this value as needed
+    float maxGravityStrength = 20f;
+    float gravityStrengthDecreaseRate = 2f;
+    private float gravityStrength = 9.81f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        SetGravityStrength(gravityStrength);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //when jump is pressed
-        //start a couroutine or maybe a for loop?
-        //the time hold is the gravity strength scale, set a max limit too
-    }
-
-    public IEnumerator ChangeGravityStrength(float maxStrength, float duration)
-    {
-        float initialStrength = Physics.gravity.magnitude;
-        float timeElapsed = 0f;
-        float totalLerpTime = 20f;
-
-
-        while (initialStrength <= maxStrength)
+        // Check if the jump button is pressed
+        if (Input.GetButtonDown("Jump"))
         {
-            currentStrength = Mathf.Lerp(initialStrength, maxStrength, timeElapsed / totalLerpTime);
-            SetGravityStrength(currentStrength);
-            timeElapsed += Time.deltaTime;
-            yield return null;
+            isJumpButtonPressed = true;
+            StartCoroutine(DecreaseGravityStrength());
         }
 
-        SetGravityStrength(maxStrength);
+        // Check if the jump button is released
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumpButtonPressed = false;
+        }
     }
 
-    public void SetGravityStrength(float strength)
-    {
-        Physics.gravity = gravitySwitch.gravityDir[gravitySwitch.gravityDirIndex].normalized * strength;
-    }
+    //IEnumerator DecreaseGravityStrength()
+    //{
+    //    while (isJumpButtonPressed && gravityStrength > 0f)
+    //    {
+    //        gravityStrength -= gravityStrengthDecreaseRate * Time.deltaTime;
 
-    public float GetGravityStrength()
-    {
-        return Physics.gravity.magnitude;
-    }
+    //        // Clamp gravity strength to avoid going below zero
+    //        gravityStrength = Mathf.Max(0f, gravityStrength);
+
+    //        // Apply the updated gravity strength to the Rigidbody
+    //        rb.gravityScale = gravityStrength / maxGravityStrength;
+
+    //        yield return null;
+    //    }
+    //}
 }
